a07c2058c4cfd0dc78070bf96c34d27fea2b8d51
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-06-04 19:55:38 +0200

    Create index.js

---
+++ server/api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const {mongoose} = require('mongoose');
+
+//start server
+const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/', require('../routes/authroutes'))
+
+//mongoDB connection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("mongoDB connected")
+})
+.catch((err) => {
+    console.log("failed to connect", err)
+})
+
+const port = process.env.PORT || 8000;
+const host = process.env.HOST || 'localhost'
+app.listen(port, function () {
+    console.log("Server Has Started!");
+    console.log(`Server is running at http://${host}:${port}`)
+});
