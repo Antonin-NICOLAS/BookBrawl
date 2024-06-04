ea691fcd8f8225db186c9fced56f0de716e136c0
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-06-04 20:55:55 +0200

    Add files via upload

---
+++ client/src/pages/a-propos.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import './a-propos.css'
+
+function APropos() {
+  return (
+    <>
+    <iframe src="https://v2-embednotion.com/a8c34d21f72a4a9dbc73a4b1f7373c16"></iframe>
+    </>
+  )
+}
+
+export default APropos;
