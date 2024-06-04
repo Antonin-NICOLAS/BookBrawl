3cfcdf770e786cd8fed2e6f03f70cfef4f14ea42
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-06-04 20:55:31 +0200

    Create home.jsx

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function Home() {
+
+  return (
+    <>
+      <div>
+        <h1>Home</h1>
+        <p>coucou</p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
