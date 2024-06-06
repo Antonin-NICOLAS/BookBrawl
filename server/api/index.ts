9f173473cfd4676af10208ed9e3db406b0031b4e
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-06-06 17:57:20 +0200

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -10,6 +10,12 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/', require('../routes/authroutes'))
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 //mongoDB connection
 mongoose.connect(process.env.MONGO_URL)
@@ -22,10 +28,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || 'localhost'
-
 app.listen(port, function () {
     console.log("Server Has Started!");
     console.log(`Server is running at http://${host}:${port}`)
 });
-
-module.exports = app;
