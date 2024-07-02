795160866d88355b3efe92d2f7ddb28377cab4f1
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-02 09:55:52 +0200

    Add files via upload

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const {mongoose} = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 //start server
 const app = express()
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
+//app.use(bodyParser.urlencoded({ extended: true }))
+
 //cors plugin
 app.use(cors())
 //routes
