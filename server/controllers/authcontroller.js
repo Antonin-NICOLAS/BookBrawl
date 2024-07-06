29c5173681cc1223ed533f33a491ca1aab251e3b
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-06 08:35:06 +0200

    Update authcontroller.js

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -50,6 +50,7 @@ const loginUser = async (req, res) => {
                 httpOnly: process.env.NODE_ENV === "production" ? true : false,
                 sameSite: process.env.NODE_ENV === "production" ? 'None' : '',
                 maxAge: 2 * 24 * 60 * 60 * 1000,
+                expires: Date(new Date.now() + 2 * 24 * 60 * 60 * 1000)
             }
             jwt.sign({ id: user._id, nom: user.nom, prenom: user.prenom, email: user.email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES }, (err, token) => {
                 if (err) throw err;
