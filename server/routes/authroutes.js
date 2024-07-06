3f05c94bdab88672149f4597157e9a8c6ddf349c
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-06 18:24:13 +0200

    Add files via upload

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { test, registerUser, loginUser } = require('../controllers/authcontroller')
+const { test, registerUser, loginUser, logoutUser, getProfile } = require('../controllers/authcontroller')
 
 const router = express.Router()
 
@@ -17,5 +17,9 @@ router.get('/', test)
 router.post('/register', registerUser)
 
 router.post('/login', loginUser)
+
+router.get('/logout', logoutUser)
+
+router.get('/profile', getProfile)
 
 module.exports = router
