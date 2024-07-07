aefae3394fe68c5913b3004480fb8d72707ef8af
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-07 17:43:09 +0200

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast'
+import { UserContextProvider } from './context/userContext'
 import Navbar from '../src/components/navbar';
 import Home from '../src/pages/home';
+import Classement from '../src/pages/classement';
+import Books from '../src/pages/books';
+import Predictions from '../src/pages/predictions'
 import APropos from '../src/pages/a-propos';
 import ErrorPage from '../src/pages/error'
 import LoginPopup from '../src/components/popup-login';
-import { UserContextProvider } from './context/userContext'
 
-axios.defaults.baseURL = process.env.NODE_ENV === "production" ? '' : process.env.BACKEND_SERVER, 
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? '' : process.env.BACKEND_SERVER, //ne rien mettre sur github
 axios.defaults.withCredentials = true
 
 export default function App() {
@@ -40,6 +43,9 @@ export default function App() {
             <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
             <Routes location={background || location}>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/ranking" element={<Classement />} />
+                <Route exact path="/books" element={<Books />} />
+                <Route exact path="/predict" element={<Predictions />} />
                 <Route path="/about" element={<APropos />} />
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
@@ -47,6 +53,12 @@ export default function App() {
                 <Routes>
                     <Route path="/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
                     <Route path="/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/ranking/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/ranking/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/books/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/books/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/predict/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    <Route path="/predict/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
                     <Route path="/about/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
                     <Route path="/about/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
                 </Routes>
