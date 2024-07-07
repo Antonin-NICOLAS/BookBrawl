17132a6441ba238c676b64e371603c31bb114d5f
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-07 22:30:17 +0200

    Add files via upload

---
+++ client/src/pages/accounts.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { UserContext } from "../context/userContext"
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import './accounts.css'
+
+function Compte() {
+    const { user, setUser } = useContext(UserContext)
+
+      //logout
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(process.env.NODE_ENV === "production" ? '/api/logout' : '/logout', {
+        withCredentials: true,
+        headers: {'Content-Type': 'application/json'
+          }
+      });
+      console.log(response);
+
+      if (response.error) {
+        toast.error('La déconnexion a échouée.');
+        console.log(error)
+      } else {
+        setUser(null);
+        toast.success('Vous êtes déconnecté !')
+        setTimeout(() => {
+            window.location.href = '/';
+          }, 2000);
+      }
+    } catch (err) {
+      toast.error('Un problème est survenu pendant la déconnection.')
+      console.log(err.response)
+    }
+  };
+
+    return (
+        <>
+            <p>{user.prenom}</p>
+            <p>{user.nom}</p>
+            <p>{user.email}</p>
+            <button className="logout" onClick={handleLogout}><i class="fa-solid fa-right-from-bracket"></i>&nbsp;&nbsp;&nbsp;Se déconnecter</button>
+        </>
+    );
+}
+
+export default Compte;
