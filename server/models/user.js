40c4d78eb6d140f589d47d854ffb6ebffdb9abdf
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-08 03:33:37 +0200

    Add files via upload

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,8 @@ const userSchema = new Schema({
     },
     nom: {
         type: String,
-        required: true
+        required: true,
+        uppercase: true
     },
     email: {
         type: String,
