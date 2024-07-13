2adce601f11d435cff0c03470ad5c32ab414838e
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-13 02:18:47 +0200

    Add files via upload

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -1,26 +1,37 @@
+import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import { createContext, useState, useEffect } from 'react';
 
-export const UserContext = createContext({})
+export const UserContext = createContext({});
 
-export function UserContextProvider({children}) {
-
+export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true); // Added isLoading state
 
     useEffect(() => {
-        if(!user) {
-            axios.get(process.env.NODE_ENV === "production" ? '/api/profile' : '/profile',{ ///api/profile
-                withCredentials: true,
-                headers: {'Content-Type': 'application/json'
-                }
-            }).then(({data}) => {
-                setUser(data)
-            })
+        const fetchUserProfile = async () => {
+            try {
+                const response = await axios.get(process.env.NODE_ENV === "production" ? '/api/profile' : '/profile', {
+                    withCredentials: true,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                setUser(response.data);
+            } catch (error) {
+                console.error('Error fetching user profile:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        if (!user) {
+            fetchUserProfile();
         }
-    }, [])
+    }, [user]);
+
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{ user, isLoading, setUser }}>
             {children}
         </UserContext.Provider>
-    )
+    );
 }
