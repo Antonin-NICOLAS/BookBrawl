0bfd36f99b1a727d768de4f09b81e95f617dfbb4
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-16 01:21:32 +0200

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast'
+//Context
 import { UserContextProvider } from './context/userContext'
-import Navbar from '../src/components/navbar';
-import Home from '../src/pages/home';
-import Classement from '../src/pages/classement';
-import Books from '../src/pages/books';
-import Predictions from '../src/pages/predictions'
-import APropos from '../src/pages/a-propos';
-import Accounts from '../src/pages/accounts';
-import ErrorPage from '../src/pages/error'
+import { LoadingProvider, useLoading } from './context/LoadingContext';
+//Componenets
+import LoadingAnimation from './components/loader';
+import Navbar from './components/navbar';
 import LoginPopup from '../src/components/popup-login';
 import PasswordPopup from '../src/components/popup-changepassword'
+//pages
+import BookDetails from './relative/book-details';
+import UserDetails from './relative/user-details'
+import Home from '../src/pages/home';
+import Classement from './classement/classement';
+import Books from './book/books';
+import Predictions from '../src/pages/predictions'
+import APropos from './a-propos/a-propos';
+import Accounts from './account/accounts';
+import ErrorPage from '../src/pages/error';
 
 axios.defaults.baseURL = process.env.NODE_ENV === "production" ? '' : process.env.BACKEND_SERVER, //ne rien mettre sur github
-axios.defaults.withCredentials = true
+    axios.defaults.withCredentials = true
 
 export default function App() {
+    //LOADING//
+    const { isLoading } = useLoading();
     //popup login
     const [ButtonPopup, setButtonPopup] = useState(false);
     const [formType, setFormType] = useState('login');
@@ -67,37 +76,42 @@ export default function App() {
     };
 
     return (
-        <UserContextProvider>
-            <Navbar onLoginClick={handleLoginClick} onLoginClickWhenOnRegister={handleRegistrationClick}/>
-            <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
-            <Routes location={background || location}>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/ranking" element={<Classement />} />
-                <Route exact path="/books" element={<Books />} />
-                <Route exact path="/predict" element={<Predictions />} />
-                <Route path="/about" element={<APropos />} />
-                <Route path="/accounts" element={<Accounts onPasswordClick={handlePasswordClick}/>} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-            {ButtonPopup && (
-                <Routes>
-                    <Route path="/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/ranking/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/ranking/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/books/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/books/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/predict/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/predict/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/about/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
-                    <Route path="/about/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+        <LoadingProvider>
+            <UserContextProvider>
+                {isLoading && <LoadingAnimation />}
+                <Navbar onLoginClick={handleLoginClick} onLoginClickWhenOnRegister={handleRegistrationClick} />
+                <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
+                <Routes location={background || location}>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/ranking" element={<Classement />} />
+                    <Route path="/user/:userId" element={<UserDetails />} />
+                    <Route exact path="/books" element={<Books />} />
+                    <Route path="/book/:bookId" element={<BookDetails />} />
+                    <Route exact path="/predict" element={<Predictions />} />
+                    <Route path="/about" element={<APropos />} />
+                    <Route path="/accounts" element={<Accounts onPasswordClick={handlePasswordClick} />} />
+                    <Route path="*" element={<ErrorPage />} />
                 </Routes>
-            )}
-                        {ButtonPasswordPopup && (
-                <Routes>
-                    <Route path="/accounts/changepassword" element={<PasswordPopup trigger={ButtonPasswordPopup} setTrigger={handlePopupPasswordClose} location={location} />} />
-                </Routes>
-            )}
-        </UserContextProvider>
+                {ButtonPopup && (
+                    <Routes>
+                        <Route path="/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/ranking/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/ranking/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/books/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/books/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/predict/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/predict/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/about/login" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                        <Route path="/about/register" element={<LoginPopup trigger={ButtonPopup} setTrigger={handlePopupClose} formType={formType} setFormType={setFormType} location={location} />} />
+                    </Routes>
+                )}
+                {ButtonPasswordPopup && (
+                    <Routes>
+                        <Route path="/accounts/changepassword" element={<PasswordPopup trigger={ButtonPasswordPopup} setTrigger={handlePopupPasswordClose} location={location} />} />
+                    </Routes>
+                )}
+            </UserContextProvider>
+        </LoadingProvider>
     );
 }
