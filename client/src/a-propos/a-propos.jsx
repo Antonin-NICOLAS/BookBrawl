df2a2d633dd3e32bed0c813f5dcafec0d7ee963b
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-16 16:46:27 +0200

    Add files via upload

--- client/src/a-propos/a-propos.jsx
+++ client/src/a-propos/a-propos.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Rules from '../assets/rules.pdf'
+
 import './a-propos.css'
 
 function APropos() {
   return (
     <>
+    <embed src={Rules} type="application/pdf"/>
     </>
   )
 }
