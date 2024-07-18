7260526216003ea73202d2a8bf885c3770cba6d2
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-19 01:02:32 +0200

    Add files via upload

--- client/src/classement/classement.jsx
+++ client/src/classement/classement.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 //Context
 import { UserContext } from '../context/userContext';
 import { useLoading } from '../context/LoadingContext';
-import { toast } from 'react-hot-toast'
+import { toast } from 'react-hot-toast';
 //CSS
 import './classement.css';
 //LOADER//
 import LoadingAnimation from '../components/loader';
+//Graphique
+import ClassementChart from './classementchart';
 
 const Classement = () => {
     //Context
@@ -28,13 +30,12 @@ const Classement = () => {
     const fetchUsers = async () => {
         setIsLoading('users', true);
         try {
-            const response = await axios.get(process.env.NODE_ENV === "production" ? '/api/user/userranking' : '/user/userranking'
-            );
+            const response = await axios.get(process.env.NODE_ENV === "production" ? '/api/user/userranking' : '/user/userranking');
             setUsers(response.data);
 
             if (response.error) {
-                console.log(response.error)
-                toast.error(response.error)
+                console.log(response.error);
+                toast.error(response.error);
             }
 
         } catch (error) {
@@ -55,40 +56,43 @@ const Classement = () => {
 
     const isUsersLoading = loadingStates.users;
 
-
     return (
         <div className='classement'>
             <h1>Classement des utilisateurs par nombre de mots lus</h1>
             {isUsersLoading ? (
                 <LoadingAnimation />
             ) : (
-                <table className='classement-table'>
-                    <thead className="table-head">
-                        <tr>
-                            <th className="column1">Rang</th>
-                            <th className="column2">Nom</th>
-                            <th className="column3">Mots lus</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users && users.length > 0 ? (
-                            users.map((userItem, index) => (
-                                <tr
-                                    key={userItem._id}
-                                    className={user && String(userItem._id) === String(user.id) ? 'table-highlight' : ''}
-                                >
-                                    <td className='column1'>{index + 1}</td>
-                                    <td className="column2"><Link to={`/user/${userItem._id}`}>{userItem.prenom} {userItem.nom}</Link></td>
-                                    <td className="column3">{userItem.wordsRead}</td>
-                                </tr>
-                            ))
-                        ) : (
+                <>
+                    <table className='classement-table'>
+                        <thead className="table-head">
                             <tr>
-                                <td colSpan="3">Aucun utilisateur trouvé</td>
+                                <th className="column1">Rang</th>
+                                <th className="column2">Nom</th>
+                                <th className="column3">Mots lus</th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>)}
+                        </thead>
+                        <tbody>
+                            {users && users.length > 0 ? (
+                                users.map((userItem, index) => (
+                                    <tr
+                                        key={userItem._id}
+                                        className={user && String(userItem._id) === String(user.id) ? 'table-highlight' : ''}
+                                    >
+                                        <td className='column1'>{index + 1}</td>
+                                        <td className="column2"><Link to={`/user/${userItem._id}`}>{userItem.prenom} {userItem.nom}</Link></td>
+                                        <td className="column3">{userItem.wordsRead}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="3">Aucun utilisateur trouvé</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    <ClassementChart users={users} className="chart"/>
+                </>
+            )}
         </div>
     );
 };
