c1b4b92b1dbe062b41506679747422fa8b404371
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-21 23:27:46 +0200

    Add files via upload

--- server/controllers/admincontroller.js
+++ server/controllers/admincontroller.js
@@ -18,10 +18,10 @@ const checkAdmin = (req, res) => {
 // Récupérer les livres en attente de vérification
 const getUnverifiedBooks = async (req, res) => {
     try {
-        const books = await Book.find({ isVerified: false });
+        const books = await Book.find({ isVerified: false }).populate('pastReaders', 'prenom');
         res.status(200).json(books);
     } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des livres en attente de vérification.' });
+        res.status(500).json({ error: 'Erreur lors de la récupération des livres en attente de vérification.' });
     }
 };
 
@@ -32,8 +32,34 @@ const verifyBook = async (req, res) => {
         await Book.findByIdAndUpdate(bookId, { isVerified: true });
         res.status(200).json({ message: 'Livre vérifié avec succès.' });
     } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la vérification du livre.' });
+        res.status(500).json({ error: 'Erreur lors de la vérification du livre.' });
     }
 };
 
-module.exports = {checkAdmin, verifyBook, getUnverifiedBooks}
+// Route pour mettre à jour les informations d'un livre
+const updateBook = async (req, res) => {
+    try {
+        const { title, author, wordsRead } = req.body;
+        const { bookId } = req.params;
+
+        // Trouver le livre par ID
+        const book = await Book.findById(bookId);
+        if (!book) {
+            return res.json({ error: "Livre non trouvé" });
+        }
+
+        // Mettre à jour les informations du livre
+        book.title = title || book.title;
+        book.author = author || book.author;
+        book.wordsRead = wordsRead || book.wordsRead;
+        book.isVerified = true;
+
+        await book.save();
+        res.status(200).json(book);
+    } catch (error) {
+        console.error("Erreur lors de la mise à jour du livre :", error);
+        res.status(500).json({ error: "Erreur lors de la mise à jour du livre" });
+    }
+};
+
+module.exports = {checkAdmin, verifyBook, getUnverifiedBooks, updateBook }
