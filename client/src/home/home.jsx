b7f8aee38a8a301e22cdfa7ca1c5aee75deb7249
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-23 01:22:09 +0200

    Add files via upload

--- client/src/home/home.jsx
+++ client/src/home/home.jsx
@@ -9,7 +9,6 @@ function Home() {
       <iframe
         src="https://rw0wp9wywkxqqp.embednotionpage.com/Book-Brawl-a8c34d21f72a4a9dbc73a4b1f7373c16?pvs=4"
       ></iframe>
-      <Footer />
     </>
   );
 }
