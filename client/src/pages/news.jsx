9b560d80e063489f3086507446dfcb19088b3348
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-23 01:23:56 +0200

    Add files via upload

---
+++ client/src/pages/news.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+//Context
+import { UserContext } from '../context/userContext';
+import { useLoading } from '../context/LoadingContext';
+import { toast } from 'react-hot-toast';
+//CSS
+import './news.css';
+//LOADER//
+import LoadingAnimation from '../components/loader';
+
+const News = () => {
+    //Context
+    const { user, isLoading } = useContext(UserContext);
+    const { setIsLoading, loadingStates } = useLoading();
+
+    return (
+        <>
+            <div className='news'>
+                <div className="coming">
+                    <p>Coming soon</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default News;
