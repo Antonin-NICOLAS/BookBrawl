f6a0b36dc0731e2471834f500b17b73c7bcb3c04
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-22 12:10:31 +0200

    Create home.jsx

---
+++ client/src/home/home.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Footer from '../components/footer'
+import './home.css'
+
+function Home() {
+
+  return (
+    <>
+      <iframe
+        src="https://rw0wp9wywkxqqp.embednotionpage.com/Book-Brawl-a8c34d21f72a4a9dbc73a4b1f7373c16?pvs=4"
+      ></iframe>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
