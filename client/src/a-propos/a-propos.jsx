d64c9fb75a4a8e159aab2c6b44881c83c4c30e67
Antonin-NICOLAS <126879419+Antonin-NICOLAS@users.noreply.github.com>
2024-07-23 01:25:33 +0200

    Add files via upload

--- client/src/a-propos/a-propos.jsx
+++ client/src/a-propos/a-propos.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Rules from '../assets/rules.pdf'
-import Footer from '../components/footer'
 
 import './a-propos.css'
 
@@ -8,7 +7,6 @@ function APropos() {
   return (
     <>
       <embed src={Rules} type="application/pdf" />
-      <Footer />
     </>
   )
 }
